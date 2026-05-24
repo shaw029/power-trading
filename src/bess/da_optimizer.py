2f4e314021c0cc8fbbaf94128e13cd8b3ff3c4fe
Abhinav Shaw <abhinavshaw.iitd@gmail.com>
2026-05-24 02:17:15 +0100

    Add degradation cost and terminal SoC constraint to DA optimizer

--- src/bess/da_optimizer.py
+++ src/bess/da_optimizer.py
@@ -17,11 +17,14 @@ def optimize_da_schedule(
     soc = [pulp.LpVariable(f"soc_{h}", lowBound=0, upBound=asset.capacity_mwh) for h in range(n_hours + 1)]
 
     prob += pulp.lpSum(
-        (discharge[h] - charge[h]) * da_prices[h] for h in hours
+        (discharge[h] - charge[h]) * da_prices[h]
+        - (discharge[h] + charge[h]) * asset.degradation_cost_per_mwh
+        for h in hours
     )
 
     initial_soc = asset.capacity_mwh * asset.initial_soc_pct
     prob += soc[0] == initial_soc
+    prob += soc[n_hours] == initial_soc
 
     for h in hours:
         prob += soc[h + 1] == soc[h] - discharge[h] + charge[h] * asset.round_trip_efficiency
